2cbc33fc8a9428350b41ec7a0b1025496cb31e3d
jrodlop140 <jrodlop140@iescarillo.es>
2024-09-25 15:01:15 +0200

    25/09/2024

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
 console.log("Hola Mundo");
+
+/**
+ * Tipos de datos:
+ * 
+ * string
+ * number - enteros y decimales
+ * boolean - true / false
+ * null
+ * undefined
+ */
+var var1: boolean = true;
+var var2: string = "Hola";
+
+var nombre: string = "Julio";//Variable global
+let apellido: string = "Rodríguez";//Variable local
+let a: string = "Pepe", b: string = "Paco", c: number = 45;
+const PI: number = 3.1415;
+
+console.log("Hola " + "Mundo");
+console.log(nombre + " " + apellido);
+console.log(nombre, apellido);
+console.log(`Hola ${nombre}`)
+console.log(`Hola ${a}`, "y", `${b}`);
+
+//Declaración de arrays
+let alumnos1A: string[] = ["Juan", "Jose", "Alex"];
+let alumnos2A: string[] = ["Fran"];
+//Forma de propagar arrays
+let alumnos:string[] = [...alumnos1A, ...alumnos2A, "Pablo"];
+//Array con diferentes datos
+let datos: (string | number | boolean)[] = ["Hola", 2, true];
+console.log(alumnos);
